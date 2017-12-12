ace2cd6f77ca67bd4eee3c126b97159c126885bd
Vaibhav Patel <vp932712@gmail.com>
2017-12-12 21:12:00 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,36 @@
 # Write your code here.
+def take_a_number(array, name)
+  array.push(name)
+  position = array.length
+  "Welcome, #{name}. You are number #{position} in line."
+
+end
+
+take_a_number(katz_deli, "vaibhav")
+take_a_number(katz_deli, "harsh")
+
+def line(array)
+  empty = []
+  if array.length<1
+    "The line is currently empty."
+  else
+  array.each_with_index do |name, index|
+    empty.push("#{index+1}. #{name}")
+
+  end
+  "The line is currently: " + empty.join(", ")
+end
+end
+
+
+
+def now_serving(array)
+  if array.length<1
+puts "There is nobody waiting to be served!"
+else
+
+  puts "Currently serving " + array.shift+"."
+
+end
+
+end
